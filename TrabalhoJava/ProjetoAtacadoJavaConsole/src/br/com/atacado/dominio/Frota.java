0976df028eaf337c38e3d72080389bd9f38a4c66
Isabela Andrade <85091218+iambelaandrade@users.noreply.github.com>
2023-04-01 00:07:55 -0400

    Update Frota.java

--- TrabalhoJava/ProjetoAtacadoJavaConsole/src/br/com/atacado/dominio/Frota.java
+++ TrabalhoJava/ProjetoAtacadoJavaConsole/src/br/com/atacado/dominio/Frota.java
@@ -4,21 +4,25 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Frota extends BaseTransporte {
+    
     private List<Veiculo> veiculo;
 
     public List<Veiculo> getVeiculo() {
+        
         return veiculo;
     }
 
     public void setVeiculo(List<Veiculo> veiculo) {
+        
         this.veiculo = veiculo;
     }
 
     public Frota() {
-        super();
+  
     }
 
     public Frota(int codigo, String descricao, LocalDate dataInsert) {
+        
         super(codigo, descricao, dataInsert);
     }
 

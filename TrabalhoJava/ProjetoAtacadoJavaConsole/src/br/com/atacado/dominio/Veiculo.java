0836b51dc045d652afb7318a3c3e9970088a36ba
Isabela Andrade <85091218+iambelaandrade@users.noreply.github.com>
2023-04-01 00:06:55 -0400

    Update Veiculo.java

--- TrabalhoJava/ProjetoAtacadoJavaConsole/src/br/com/atacado/dominio/Veiculo.java
+++ TrabalhoJava/ProjetoAtacadoJavaConsole/src/br/com/atacado/dominio/Veiculo.java
@@ -3,16 +3,19 @@ package br.com.atacado.dominio;
 import java.time.LocalDate;
 
 public class Veiculo extends Frota {
+   
    private int codigoFrota;
    private String placa;
 
    public Veiculo() {
-      super();
+   
    }
 
    public Veiculo(int codigo, String descricao, LocalDate dataInsert, int codigoFrota) {
+      
       super(codigo, descricao, dataInsert);
       this.codigoFrota = codigoFrota;
+      
    }
 
 }
